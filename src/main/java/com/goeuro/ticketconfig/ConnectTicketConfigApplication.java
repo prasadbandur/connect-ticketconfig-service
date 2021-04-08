3420e7876632673ecf14ea18fd8a92b063b8091b
prasad <renuka.bandur@omio.com>
2021-04-08 13:45:47 +0200

    FND-2201: Initial ticket config changes

---
+++ src/main/java/com/goeuro/ticketconfig/ConnectTicketConfigApplication.java
@@ -0,0 +1,13 @@
+package com.goeuro.ticketconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectTicketConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectTicketConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goeuro/ticketconfig/ConnectTicketConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goeuro.ticketconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectTicketConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
